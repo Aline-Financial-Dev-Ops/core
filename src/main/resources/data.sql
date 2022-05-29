f7ac9e0784b9bc9ca5041496fb418c31a4509f73
leandroyabut <leandro.yabut@smoothstack.com>
2022-05-29 13:08:44 -0600

    Add default merchants to data.sql.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,11 @@ VALUES (1, '123 Aline Financial St.', 'New York', 'New York', '10001', '12345678
 INSERT IGNORE INTO branch (id, name, phone, address, city, state, zipcode, bank_id)
 VALUES (1, 'Main Branch', '(800) 123-4567', '123 Aline Financial St.', 'New York', 'New York', '10001', 1);
 
+INSERT IGNORE INTO merchant (code, name)
+VALUES ('ALINE', 'Aline Financial');
+
+INSERT IGNORE INTO merchant (code, name)
+VALUES ('NONE', 'None');
 /*********************************************************************
 ******************* USAGE WITH YOUR PROJECT **************************
 **********************************************************************
