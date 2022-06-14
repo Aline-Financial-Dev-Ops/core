c111e44a95b3e2a60a1bd4be73bea844f8382685
leandroyabut <leandro.yabut@smoothstack.com>
2022-06-13 22:05:22 -0600

    AF-303 Add CreditCardAccount model.

---
+++ src/main/java/com/aline/core/model/account/CreditCardAccount.java
@@ -0,0 +1,33 @@
+package com.aline.core.model.account;
+
+import com.aline.core.model.credit.CreditLine;
+import com.aline.core.model.payment.Payment;
+import com.aline.core.model.payment.PaymentRecord;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@DiscriminatorValue(AccountType.Values.CREDIT_CARD)
+public class CreditCardAccount extends Account {
+    @OneToOne
+    private CreditLine creditLine;
+    @OneToMany
+    private List<Payment> payments;
+    @OneToMany
+    private List<PaymentRecord> paymentHistory;
+}
