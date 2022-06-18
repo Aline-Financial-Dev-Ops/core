5abf4411aeaa06ec92e1fe47586dd2b26ae24f4e
leandroyabut <leandro.yabut@smoothstack.com>
2022-06-18 01:35:34 -0600

    AF-303 Update core.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,11 +5,27 @@ VALUES (1, '123 Aline Financial St.', 'New York', 'New York', '10001', '12345678
 INSERT IGNORE INTO branch (id, name, phone, address, city, state, zipcode, bank_id)
 VALUES (1, 'Main Branch', '(800) 123-4567', '123 Aline Financial St.', 'New York', 'New York', '10001', 1);
 
+create table if not exists merchant
+(
+    code          varchar(8)   not null
+        primary key,
+    address       varchar(255) null,
+    city          varchar(255) null,
+    description   varchar(255) null,
+    name          varchar(150) not null,
+    registered_at datetime(6)  null,
+    state         varchar(255) null,
+    zipcode       varchar(255) null
+);
+
 INSERT IGNORE INTO merchant (code, name)
 VALUES ('ALINE', 'Aline Financial');
 
 INSERT IGNORE INTO merchant (code, name)
 VALUES ('NONE', 'None');
+
+INSERT IGNORE INTO card_issuer (issuer_name, card_number_length)
+VALUES ('AMEX', 16);
 
 INSERT IGNORE INTO card_issuer (issuer_name, card_number_length)
 VALUES ('VISA', 16);
@@ -67,9 +83,6 @@ VALUES (649, 'DISCOVER');
 
 INSERT IGNORE INTO issuer_identification_number (iin, card_issuer_name)
 VALUES (65, 'DISCOVER');
-
-INSERT IGNORE INTO card_issuer (issuer_name, card_number_length)
-VALUES ('AMEX', 16);
 
 INSERT IGNORE INTO credit_card_offer (amount, offer_name, description, card_issuer_name, credit_line_type, min_apr, max_apr, min_payment)
 VALUES (500000, 'Aline Standard', 'Standard credit card.', 'VISA', 'STANDARD', 10.5, 24.99, 25);
